0dbd008b430b010dc9a7321c0399b178af17969b
Federico Dotti <fede.dotti.94@gmail.com>
2026-03-30 20:39:59 +0000

    Move state data to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,120 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import "../styles/App.css"
 import Education from "./Education.jsx"
 import Experience from "./Experience.jsx"
 import "./GeneralInfo.jsx"
 import GeneralInfo from "./GeneralInfo.jsx"
 import Summary from "./Summary.jsx"
+import React from 'react'
+
+let nextId = 1;
 
 function App() {
+
+  const [experienceArray, setExperienceArray] = useState([
+    {
+      id: 0,
+      companyName: '',
+      jobTitle: '',
+      startDate: '',
+      endDate: '',
+      jobDescription: ''
+    }
+  ])
+
+  const experienceList = experienceArray.map((job, index) =>
+    <React.Fragment key={job.id}>
+      {index > 0 && <hr />}
+      <div className='grid'>
+        <span>Company Name: {job.companyName}</span>
+        <span>Job Title: {job.jobTitle}</span>
+        <span>Start Date: {job.startDate}</span>
+        <span>End Date: {job.endDate}</span>
+      </div>
+    </React.Fragment>
+  )
+
+  console.log(experienceList);
+
+  function handleCompanyNameChange(e) {
+    console.log(e.target.className.split('-').at(-1));
+    const reactKey = Number(e.target.className.split('-').at(-1));
+    const job = experienceArray.filter(job => job.id === reactKey);
+    const remainingJobs = experienceArray.filter(job => job.id !== reactKey);
+    console.log(job[0], remainingJobs)
+    job[0].companyName = e.target.value;
+    setExperienceArray([
+      ...remainingJobs, job[0]
+    ].sort((a, b) => a.id - b.id))
+  }
+
+  function handleJobTitleChange(e) {
+    const reactKey = Number(e.target.className.split('-').at(-1));
+    const job = experienceArray.filter(job => job.id === reactKey);
+    const remainingJobs = experienceArray.filter(job => job.id !== reactKey);
+    job[0].jobTitle = e.target.value;
+    setExperienceArray([
+      ...remainingJobs, job[0]
+    ].sort((a, b) => a.id - b.id))
+  }
+
+  function handleStartDateChange(e) {
+    const reactKey = Number(e.target.className.split('-').at(-1));
+    const job = experienceArray.filter(job => job.id === reactKey);
+    const remainingJobs = experienceArray.filter(job => job.id !== reactKey);
+    job[0].startDate = e.target.value;
+    setExperienceArray([
+      ...remainingJobs, job[0]
+    ].sort((a, b) => a.id - b.id))
+  }
+
+  function handleEndDateChange(e) {
+    const reactKey = Number(e.target.className.split('-').at(-1));
+    const job = experienceArray.filter(job => job.id === reactKey);
+    const remainingJobs = experienceArray.filter(job => job.id !== reactKey);
+    job[0].endDate = e.target.value;
+    setExperienceArray([
+      ...remainingJobs, job[0]
+    ].sort((a, b) => a.id - b.id))
+  }
+
+  function handleJobDescriptionChange(e) {
+    const reactKey = Number(e.target.className.split('-').at(-1));
+    const job = experienceArray.filter(job => job.id === reactKey);
+    const remainingJobs = experienceArray.filter(job => job.id !== reactKey);
+    job[0].jobDescription = e.target.value;
+    setExperienceArray([
+      ...remainingJobs, job[0]
+    ].sort((a, b) => a.id - b.id))
+  }
+
+  function handleAddExperienceClick(e) {
+    e.preventDefault();
+    const job = {
+      id: nextId,
+      companyName: '',
+      jobTitle: '',
+      startDate: '',
+      endDate: '',
+      jobDescription: ''
+    }
+
+    ++nextId;
+
+    setExperienceArray([
+      ...experienceArray, job
+    ])
+  }
+
+  function handleRemoveJobClick(e) {
+    e.preventDefault();
+    const reactKey = Number(e.target.className.split('-').at(-1));
+    const newExperienceArray = experienceArray.filter(job => job.id !== reactKey);
+    setExperienceArray(newExperienceArray);
+  }
+
+  const dialogRef = useRef(null);
+
 
   return (
     <>
@@ -17,9 +125,32 @@ function App() {
           <GeneralInfo/>
           <Summary/>
           <Education/>
-          <Experience/>
+          <Experience
+            experienceArray={experienceArray}
+            handleCompanyNameChange={handleCompanyNameChange}
+            handleJobTitleChange={handleJobTitleChange}
+            handleStartDateChange={handleStartDateChange}
+            handleEndDateChange={handleEndDateChange}
+            handleJobDescriptionChange={handleJobDescriptionChange}
+            handleAddExperienceClick={handleAddExperienceClick}
+            handleRemoveJobClick={handleRemoveJobClick}
+          />
+          <button onClick={(e) => {
+            e.preventDefault();
+            dialogRef.current.showModal();
+            // window.print();
+            // dialogRef.current.close();
+          }}>
+            Print CV
+          </button>
         </div>
       </form>
+      <dialog ref={dialogRef}>
+        <div>
+          <div className="section-header">Experience</div>
+          {experienceList}
+        </div>
+      </dialog>
     </>
   )
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,65 +1,77 @@
 import "../styles/section.css"
 import { useState } from 'react'
 import React from 'react'
+export { Experience, }
 
-function Experience() {
-  const [experienceArray, setExperienceArray] = useState([
-    {
-      id: 0,
-      companyName: '',
-      jobTitle: '',
-      startDate: '',
-      endDate: '',
-      jobDescription: ''
-    }
-  ])
-
-  const experienceList = experienceArray.map(job => 
+function Experience({
+  experienceArray,
+  handleCompanyNameChange,
+  handleJobTitleChange,
+  handleStartDateChange,
+  handleEndDateChange,
+  handleJobDescriptionChange,
+  handleAddExperienceClick,
+  handleRemoveJobClick
+}) {
+  const experienceList = experienceArray.map((job, index) => 
     <React.Fragment key={job.id}>
+      {index > 0 && <hr />} 
+      <button className={`react-key-${job.id}`} onClick={handleRemoveJobClick}>
+        Remove job
+      </button>
       <div>
         <label>Company name: <br />
-          <input type="text" value={job.companyName}/>
+          <input 
+            className={`react-key-${job.id}`}
+            type="text" 
+            value={job.companyName}
+            onChange={handleCompanyNameChange}
+          />
         </label>
         <label>Job title: <br />
-          <input type="text" value={job.jobTitle} />
+          <input
+            className={`react-key-${job.id}`} 
+            type="text" 
+            value={job.jobTitle} 
+            onChange={handleJobTitleChange}
+          />
         </label>
         <label>Start date: <br />
-          <input type="date" value={job.startDate}/>
+          <input
+            className={`react-key-${job.id}`} 
+            type="date" 
+            value={job.startDate}
+            onChange={handleStartDateChange}
+          />
         </label>
         <label>End date: <br />
-          <input type="date" value={job.endDate}/>
+          <input
+            className={`react-key-${job.id}`} 
+            type="date" 
+            value={job.endDate}
+            onChange={handleEndDateChange}
+          />
         </label>
-        <label>Job description: <br />
-          <textarea name={`jobdescription-${job.id}`} id="" value={job.jobDescription}></textarea>
+        <label className="span-2">Job description: <br />
+          <textarea
+            className={`react-key-${job.id}`} 
+            name={`jobdescription-${job.id}`} 
+            id="" 
+            value={job.jobDescription}
+            onChange={handleJobDescriptionChange}>
+          </textarea>
         </label>
       </div>
-      <hr />
     </React.Fragment>
   );
 
   return (
-    <div className="section">
+    <div className="section experience">
       <div>Experience</div>
-      {/* <div>
-        <label>Company name: <br />
-          <input type="text" />
-        </label>
-        <label>Job title: <br />
-          <input type="text" />
-        </label>
-        <label>Start date: <br />
-          <input type="date" />
-        </label>
-        <label>End date: <br />
-          <input type="date" />
-        </label>
-        <label>Job description: <br />
-          <textarea name="jobdescription-1" id=""></textarea>
-        </label>
-      </div> */}
       {experienceList}
-      <hr />
-      <button>Add new job experience</button>
+      <button onClick={handleAddExperienceClick}>
+        Add new job experience
+      </button>
     </div>
   )
 }
